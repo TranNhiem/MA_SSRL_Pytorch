187540ec20e35de10795620eeba06f6b8c7ef105
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2022-03-30 20:11:16 +0800

    cleaning_repository

--- solo/losses/__init__.py
+++ solo/losses/__init__.py
@@ -17,32 +17,11 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-from solo.losses.barlow import barlow_loss_func
 from solo.losses.byol import byol_loss_func
-from solo.losses.deepclusterv2 import deepclusterv2_loss_func
-from solo.losses.dino import DINOLoss
-from solo.losses.moco import moco_loss_func
-from solo.losses.nnclr import nnclr_loss_func
-from solo.losses.ressl import ressl_loss_func
-from solo.losses.simclr import simclr_loss_func
-from solo.losses.simsiam import simsiam_loss_func
-from solo.losses.swav import swav_loss_func
-from solo.losses.vibcreg import vibcreg_loss_func
-from solo.losses.vicreg import vicreg_loss_func
-from solo.losses.wmse import wmse_loss_func
+
 
 __all__ = [
-    "barlow_loss_func",
+
     "byol_loss_func",
-    "deepclusterv2_loss_func",
-    "DINOLoss",
-    "moco_loss_func",
-    "nnclr_loss_func",
-    "ressl_loss_func",
-    "simclr_loss_func",
-    "simsiam_loss_func",
-    "swav_loss_func",
-    "vibcreg_loss_func",
-    "vicreg_loss_func",
-    "wmse_loss_func",
+
 ]
--- solo/methods/__init__.py
+++ solo/methods/__init__.py
@@ -17,72 +17,27 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-from solo.methods.barlow_twins import BarlowTwins
 from solo.methods.base import BaseMethod
 from solo.methods.byol import BYOL
 from solo.methods.massl import MASSL
 from solo.methods.massl_edit import MASSL_edit
-from solo.methods.deepclusterv2 import DeepClusterV2
-from solo.methods.dino import DINO
-from solo.methods.linear import LinearModel
-from solo.methods.mocov2plus import MoCoV2Plus
-from solo.methods.nnbyol import NNBYOL
-from solo.methods.nnclr import NNCLR
-from solo.methods.nnsiam import NNSiam
-from solo.methods.ressl import ReSSL
-from solo.methods.simclr import SimCLR
-from solo.methods.simsiam import SimSiam
-from solo.methods.supcon import SupCon
-from solo.methods.swav import SwAV
-from solo.methods.vibcreg import VIbCReg
-from solo.methods.vicreg import VICReg
-from solo.methods.wmse import WMSE
+
 
 METHODS = {
     # base classes
     "base": BaseMethod,
-    "linear": LinearModel,
     # methods
-    "barlow_twins": BarlowTwins,
     "byol": BYOL,
     "massl":MASSL,
     "massl_edit":MASSL_edit,
-    "deepclusterv2": DeepClusterV2,
-    "dino": DINO,
-    "mocov2plus": MoCoV2Plus,
-    "nnbyol": NNBYOL,
-    "nnclr": NNCLR,
-    "nnsiam": NNSiam,
-    "ressl": ReSSL,
-    "simclr": SimCLR,
-    "simsiam": SimSiam,
-    "supcon": SupCon,
-    "swav": SwAV,
-    "vibcreg": VIbCReg,
-    "vicreg": VICReg,
-    "wmse": WMSE,
+
 }
 __all__ = [
-    "BarlowTwins",
+
     "BYOL",
     "MASSL",
     "MASSL_edit",
     "BaseMethod",
-    "DeepClusterV2",
-    "DINO",
-    "LinearModel",
-    "MoCoV2Plus",
-    "NNBYOL",
-    "NNCLR",
-    "NNSiam",
-    "ReSSL",
-    "SimCLR",
-    "SimSiam",
-    "SupCon",
-    "SwAV",
-    "VIbCReg",
-    "VICReg",
-    "WMSE",
 ]
 
 try:
