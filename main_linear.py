87d946461850240fdd54de761603f13ef3710c2b
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2022-05-15 22:37:43 +0800

    configure

--- main_linear.py
+++ main_linear.py
@@ -1,6 +1,6 @@
 
 # 2022 Tran_Nhiem, Chi-En Huang (SSL Team)
-# Partially Inherence from 2021 solo-learn team development.
+#  Inherence from 2021 solo-learn team development.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
