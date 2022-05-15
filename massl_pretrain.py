769b835d50e1201c484d55b8b84c7035a12c535d
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2022-05-15 14:27:10 +0800

    cleaning_repository

--- massl_pretrain.py
+++ massl_pretrain.py
@@ -1,4 +1,5 @@
-# Copyright 2021 solo-learn development team.
+# 2022 Tran_Nhiem, Chi-En Huang (SSL Team)
+# Pre-training Partially Inherence from 2021 solo-learn team development.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
@@ -10,12 +11,6 @@
 # The above copyright notice and this permission notice shall be included in all copies
 # or substantial portions of the Software.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
 
 import os
 from pprint import pprint
@@ -202,7 +197,6 @@ def main():
     #     #x1, x2, x3, x4 = im
     #     print(x1.shape)
     #     break
-
 
     if args.dali:
         trainer.fit(model, val_dataloaders=val_loader, ckpt_path=ckpt_path)
